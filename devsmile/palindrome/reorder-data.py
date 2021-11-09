ce28b432e07accc675fff5b2bb4d1132a5c82fa3
Min Kyu Kim <devsmile@twc.co.kr>
2021-11-09 11:51:59 +0900

    update code

--- devsmile/palindrome/reorder-data.py
+++ devsmile/palindrome/reorder-data.py
@@ -1,7 +1,10 @@
-from typing import List
-
-
-logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
-
-def solution(logs: List[str]) -> List:
-    pass
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        letters, digits = [], []
+        for l in logs:
+            if l.split()[1].isdigit():
+                digits.append(l)
+            else:
+                letters.append(l)
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        return letters + digits
