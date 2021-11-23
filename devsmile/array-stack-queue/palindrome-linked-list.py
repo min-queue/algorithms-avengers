351a4f9c529f962ed3f5476049f607c1d82dc5b8
Min Kyu Kim <devsmile@twc.co.kr>
2021-11-23 14:14:52 +0900

    linked-palindrome

---
+++ devsmile/array-stack-queue/palindrome-linked-list.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        q = []
+        node = head
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+        
+        while len(q) > 1:
+            if q.pop(0) != q.pop():
+                return False
+        return True
