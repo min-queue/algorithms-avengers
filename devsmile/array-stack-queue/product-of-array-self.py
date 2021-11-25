95cea3dbb10b0cabd4437130c0e92f6908ab7eb7
Min Kyu Kim <devsmile@twc.co.kr>
2021-11-25 11:54:01 +0900

    swap-nodes-in-pairs recursive version

---
+++ devsmile/array-stack-queue/product-of-array-self.py
@@ -0,0 +1,16 @@
+# Example 1:
+
+# Input: nums = [1,2,3,4]
+# Output: [24,12,8,6]
+# Example 2:
+
+# Input: nums = [-1,1,0,-3,3]
+# Output: [0,0,9,0,0]
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pass
+
+
+s = Solution()
+s.productExceptSelf(nums = [-1,1,0,-3,3])
--- devsmile/linked-list/swap-nodes-in-pairs.py
+++ devsmile/linked-list/swap-nodes-in-pairs.py
@@ -7,16 +7,28 @@ class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         root = prev = ListNode(None)
         prev.next = head
-        
+
         while head and head.next:
             tmp = head.next
             head.next = tmp.next
             tmp.next = head
-            
+
             prev.next = tmp
-            
+
             head = head.next
             prev = prev.next.next
-            
+
         return root.next
-            
+
+
+
+# Recursive Solution
+class Solution_Recursive:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head and head.next:
+            tmp = head
+            head = head.next
+            tmp.next = self.swapPairs(head.next)
+            head.next = tmp
+        return head
+
