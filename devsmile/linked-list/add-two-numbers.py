a415cb766c1cf90330838ef1ab15fbb246436548
Min Kyu Kim <devsmile@twc.co.kr>
2021-11-25 11:55:16 +0900

    swap-nodes-in-pairs recursive version

--- devsmile/linked-list/add-two-numbers.py
+++ devsmile/linked-list/add-two-numbers.py
@@ -3,28 +3,31 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
+
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         next_sum = 0
         tail = None
         head = None
-        while l1 is not None  or l2 is not None :
-            
+        while l1 is not None or l2 is not None:
+
             if l1 is not None and l2 is not None:
                 sum_number = l1.val + l2.val + next_sum
                 l1 = l1.next
                 l2 = l2.next
-            
+
             elif l1 is not None:
-                sum_number = l1.val +  next_sum
+                sum_number = l1.val + next_sum
                 l1 = l1.next
-            
+
             elif l2 is not None:
-                sum_number = l2.val +  next_sum
+                sum_number = l2.val + next_sum
                 l2 = l2.next
-            
-            next_sum = sum_number//10
+
+                next_sum = sum_number // 10
 
                 head = ListNode(val=sum_number % 10)
                 tail = head
@@ -32,9 +35,8 @@ class Solution:
                 tail.next = ListNode(val=sum_number % 10)
                 tail = tail.next
 
-        if next_sum != 0 :
+        if next_sum != 0:
             tail.next = ListNode(val=next_sum)
             tail = tail.next
-                
-                
+
         return head
