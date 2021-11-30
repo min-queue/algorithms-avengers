825df710cdc21f1c3e0b60edece1de610730ed1e
Min Kyu Kim <devsmile@twc.co.kr>
2021-11-30 12:03:03 +0900

    valid-parentheses problem end

---
+++ devsmile/stack-queue/valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        my_stack = []
+        my_dict = {')':'(', ']':'[', '}': '{'}
+        if len(s) <= 1:
+            return False
+
+        for e in s:
+            if e in ['(','{','[']:
+                my_stack.append(e)
+            elif e in [')','}',']'] and len(my_stack) > 0:
+                pp = my_stack.pop()
+                if pp != my_dict[e]:
+                    return False
+            else :
+                return False
+
+        if len(my_stack) > 0 :
+            return False
+
+        return True
