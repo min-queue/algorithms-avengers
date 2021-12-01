0413b713fc03f802fa03f2c52fd6c56d5d8a3101
Min Kyu Kim <devsmile@twc.co.kr>
2021-12-01 20:03:34 +0900

    wip -3

---
+++ devsmile/stack-queue/remove-duplicate-letters.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            print(suffix)
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters(suffix.replace(char,''))
+        return ''
