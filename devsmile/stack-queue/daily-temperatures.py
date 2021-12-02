5e34f7f307c272bea8341866f333e8957ee1d625
Min Kyu Kim <devsmile@twc.co.kr>
2021-12-02 16:27:19 +0900

    end stack chapters

--- devsmile/stack-queue/daily-temperatures.py
+++ devsmile/stack-queue/daily-temperatures.py
@@ -2,9 +2,9 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         result = [0] * len(temperatures)
         stack = []
-        for i , temp in enumerate(temperatures):
+        for i, temp in enumerate(temperatures):
             while stack and temp > temperatures[stack[-1]]:
                 last_idx = stack.pop()
-                result[last_idx]  = i - last_idx
+                result[last_idx] = i - last_idx
             stack.append(i)
         return result
--- devsmile/stack-queue/remove-duplicate-letters.py
+++ devsmile/stack-queue/remove-duplicate-letters.py
@@ -1,11 +1,12 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         for char in sorted(set(s)):
-            suffix = s[s.index(char):]
+            suffix = s[s.index(char) :]
             print(suffix)
             if set(s) == set(suffix):
-                return char + self.removeDuplicateLetters(suffix.replace(char,''))
-        return ''
+                return char + self.removeDuplicateLetters(suffix.replace(char, ""))
+        return ""
+
 
 a = "cbacdcbc"
 print(a[1:])
