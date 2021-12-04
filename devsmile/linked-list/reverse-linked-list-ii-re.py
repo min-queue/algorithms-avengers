f28f59f306eff9b46de2e4690a35e70df21b3299
Min Kyu Kim <devsmile@twc.co.kr>
2021-12-04 16:40:01 +0900

    sth special

--- devsmile/linked-list/reverse-linked-list-ii-re.py
+++ devsmile/linked-list/reverse-linked-list-ii-re.py
@@ -4,11 +4,14 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Solution:
-    def reverseBetween(self, head: Optional[ListNode], m: int, n: int) -> Optional[ListNode]:
+    def reverseBetween(
+        self, head: Optional[ListNode], m: int, n: int
+    ) -> Optional[ListNode]:
         root = start = ListNode(None)
         root.next = head
-        for _ in range(m-1):
+        for _ in range(m - 1):
             start = start.next
 
         end = start.next
@@ -16,4 +19,3 @@ class Solution:
             tmp, start.next, end.next = start.next, end.next, end.next.next
             start.next.next = tmp
         return root.next
-
