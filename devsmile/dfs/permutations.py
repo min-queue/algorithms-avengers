85b6224462687528cdebe4998bb726afd3f34c93
Min Kyu Kim <devsmile@twc.co.kr>
2021-12-08 12:25:06 +0900

    number-of-islands-re

---
+++ devsmile/dfs/permutations.py
@@ -0,0 +1,24 @@
+class Solution:
+    def permute(self, nums):
+        def dfs(left_nums, path):
+            if len(left_nums) == 0:
+                result.append(path)
+            else :
+                for num in left_nums:
+                    t = left_nums[:]
+                    t.remove(num)
+                    p = path[:]
+                    p.append(num)
+                    dfs(t, p)
+        result = []
+
+        for n in nums:
+            t = nums[:]
+            t.remove(n)
+
+            dfs(t, [n])
+        return result
+
+input = [1,2,3]
+
+print(Solution().permute(input))
