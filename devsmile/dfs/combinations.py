ddf2d3f829b7afc55744ccd331f40889b89bd146
Min Kyu Kim <devsmile@twc.co.kr>
2021-12-09 11:43:16 +0900

    combinations

---
+++ devsmile/dfs/combinations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def dfs(numbers, result, k):
+            if k == 0:
+                answer.append(result)
+                return
+            else:
+                for idx, num in enumerate(numbers):
+                    result_new = result[:]
+                    result_new.append(num)
+                    dfs(numbers[idx+1:], result_new, k-1)
+
+        numbers = [n for n in range(1, n+1)]
+        answer = []
+        dfs(numbers, [], k)
+        return answer
